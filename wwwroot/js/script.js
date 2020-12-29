e89c5459397da831f6efb15c8766ab732eb5d59b
Daniel Forsberg <mail@danielforsberg.se>
2020-12-29 15:44:17 +0100

    Add blazor version

---
+++ wwwroot/js/script.js
@@ -0,0 +1,20 @@
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: 58.571526, lng: 14.005951};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+
+(function (i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date(); a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-71802847-1', 'auto');
+ga('send', 'pageview');
